d0410ff5030cb7bb567d220e849f441ede481265
Omar Chavira <omarchavira@wsl.com>
2025-04-04 12:35:58 -0600

    WIP. Added the new data types (only the most basic variable types and a 2D vector to make some more tests and improvements)

--- main.c
+++ main.c
@@ -4,8 +4,36 @@
 
 int main(void)
 {
-    TYPE data = createInteger(5);
-    printf("%d\n", *((int*)(data)) );
-    deleteDataUnit(data);
+
+    TYPE shortV = createShort(5);
+    printf("%d\n", *((short *) (shortV)) );
+
+    TYPE integer = createInteger(5);
+    printf("%d\n", *((int *) (integer)) );
+
+    TYPE floatV = createFloat(5.236);
+    printf("%f\n", *((float *) floatV));
+
+    TYPE doubleV = createDouble(12.6546846);
+    printf("%f\n", *((double *) doubleV));
+
+    TYPE character = createCharacter('a');
+    printf("%c\n", *((char *) character));
+
+    TYPE vector2D = createVec2D(1.5, 2.5);
+    printf("(%f, %f)\n", (*(Vector2D*)vector2D).x, (*(Vector2D*)vector2D).y);
+
+    char *str = "Hola mundo!!\n";
+
+    TYPE string = createString(str);
+    printf("%s\n",str);
+
+    deleteDataUnit(shortV);
+    deleteDataUnit(integer);
+    deleteDataUnit(floatV);
+    deleteDataUnit(doubleV);
+    deleteDataUnit(character);  
+    deleteDataUnit(string);
+    deleteDataUnit(vector2D);
     return 0;
 }
